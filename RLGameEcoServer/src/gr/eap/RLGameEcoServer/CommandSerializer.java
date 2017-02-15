acbbd98b09bc997b36100205064355d50be9b2b3
kyriakosgi <kyriakosgi@gmail.com>
2017-02-15 23:46:30 +0200

    Changed serialized command description

--- RLGameEcoServer/src/gr/eap/RLGameEcoServer/CommandSerializer.java
+++ RLGameEcoServer/src/gr/eap/RLGameEcoServer/CommandSerializer.java
@@ -1,6 +1,6 @@
 package gr.eap.RLGameEcoServer;
 
-// The general format of a serialized command is the Json serialized Command Object with the addition of the property "className" holding the Command type name
+// The general format of a serialized command is the serialized Command Object with the addition of the Command type name info
 public interface CommandSerializer {
 	public String serialize(Command command);
 	public Command deserialize(String serializedCommand, String commandType);
