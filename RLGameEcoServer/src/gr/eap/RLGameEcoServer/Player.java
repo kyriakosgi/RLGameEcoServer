c0136d7e8be1b64e97ef6898da933f3eb4bc87a9
kyriakosgi <kyriakosgi@gmail.com>
2017-02-23 22:27:47 +0200

    Added Player abstract class

---
+++ RLGameEcoServer/src/gr/eap/RLGameEcoServer/Player.java
@@ -0,0 +1,18 @@
+package gr.eap.RLGameEcoServer;
+
+public abstract class Player extends Participant {
+	private String userName;
+	private String password;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
