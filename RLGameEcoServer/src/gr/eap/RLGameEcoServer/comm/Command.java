5024e5a70b9bb5a3ba9a352563d52c3af58f6b28
kyriakosgi <kyriakosgi@gmail.com>
2017-02-27 20:40:26 +0200

    Changed Command interface so that it inherits from CommunicationsObject

--- RLGameEcoServer/src/gr/eap/RLGameEcoServer/comm/Command.java
+++ RLGameEcoServer/src/gr/eap/RLGameEcoServer/comm/Command.java
@@ -2,7 +2,7 @@ package gr.eap.RLGameEcoServer.comm;
 
 import java.util.List;
 
-public interface Command {
+public interface Command extends CommunicationsObject{
 	public int getId();
 	public void setId(int value);
 	public List<Response> execute();
