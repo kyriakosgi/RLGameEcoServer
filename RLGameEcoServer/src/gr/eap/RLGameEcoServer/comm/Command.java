effabcc5bc1c3dd4d6729dd0e5a6d9bec2d5ccd2
kyriakosgi <kyriakosgi@gmail.com>
2017-02-27 22:43:05 +0200

    Added connection property

--- RLGameEcoServer/src/gr/eap/RLGameEcoServer/comm/Command.java
+++ RLGameEcoServer/src/gr/eap/RLGameEcoServer/comm/Command.java
@@ -2,8 +2,11 @@ package gr.eap.RLGameEcoServer.comm;
 
 import java.util.List;
 
+import org.java_websocket.WebSocket;
+
 public abstract class Command extends CommunicationsObject{
 	private int id;
+	private WebSocket connection;
 	
 
 	public int getId() {
@@ -13,6 +16,16 @@ public abstract class Command extends CommunicationsObject{
 
 	public void setId(int id) {
 		this.id = id;
+	}
+
+
+	public WebSocket getConnection() {
+		return connection;
+	}
+
+
+	public void setConnection(WebSocket connection) {
+		this.connection = connection;
 	}
 
 
