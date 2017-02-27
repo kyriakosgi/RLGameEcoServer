cb5a771c62009b96737a1c8cf3faaeb336baff33
kyriakosgi <kyriakosgi@gmail.com>
2017-02-27 20:44:09 +0200

    Changed class Response so that it implements CommunicationsObject
    Added property gameUid

--- RLGameEcoServer/src/gr/eap/RLGameEcoServer/comm/Response.java
+++ RLGameEcoServer/src/gr/eap/RLGameEcoServer/comm/Response.java
@@ -1,44 +1,67 @@
 package gr.eap.RLGameEcoServer.comm;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 import gr.eap.RLGameEcoServer.GameState;
 
-public class Response {
+public class Response implements CommunicationsObject {
 	private int userId;
+	private UUID gameUid;
 	private int commandID;
 	private ArrayList<String> availableCommands;
 	private GameState gameState;
 	private String message;
-	
+
+	@Override
 	public int getUserId() {
 		return userId;
 	}
+
+	@Override
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
+
 	public int getCommandID() {
 		return commandID;
 	}
+
 	public void setCommandID(int commandID) {
 		this.commandID = commandID;
 	}
+
 	public ArrayList<String> getAvailableCommands() {
 		return availableCommands;
 	}
+
 	public void setAvailableCommands(ArrayList<String> availableCommands) {
 		this.availableCommands = availableCommands;
 	}
+
 	public GameState getGameState() {
 		return gameState;
 	}
+
 	public void setGameState(GameState gameState) {
 		this.gameState = gameState;
 	}
+
 	public String getMessage() {
 		return message;
 	}
+
 	public void setMessage(String message) {
 		this.message = message;
+	}
+
+	@Override
+	public UUID getGameUid() {
+		return gameUid;
+	}
+
+	@Override
+	public void setGameUid(UUID gameUid) {
+		this.gameUid = gameUid;
 	}
 }
