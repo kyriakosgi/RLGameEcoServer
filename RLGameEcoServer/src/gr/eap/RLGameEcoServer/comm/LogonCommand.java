80f953bf246a56ef4fa47cd2e9f44bb4ea27e3da
kyriakosgi <kyriakosgi@gmail.com>
2017-02-27 22:45:50 +0200

    Changed inheritance
    LogonCommand now extends the Command abstract class

--- RLGameEcoServer/src/gr/eap/RLGameEcoServer/comm/LogonCommand.java
+++ RLGameEcoServer/src/gr/eap/RLGameEcoServer/comm/LogonCommand.java
@@ -6,25 +6,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import gr.eap.RLGameEcoServer.Player;
+import gr.eap.RLGameEcoServer.PlayersRegister;
 import gr.eap.RLGameEcoServer.db.MySQLHelper;
 import gr.eap.RLGameEcoServer.db.MySQLHelper.parameterValue;
 
-public class LogonCommand implements Command {
+public class LogonCommand extends Command {
 	private String userName;
 	private String password;
-	private int id;
-	private int userId;
-	private UUID gameUid;
 
-	@Override
-	public int getId() {
-		return id;
-	}
-
-	@Override
-	public void setId(int id) {
-		this.id = id;
-	}
 
 	public String getUserName() {
 		return userName;
@@ -44,6 +34,7 @@ public class LogonCommand implements Command {
 
 	@Override
 	public List<Response> execute() {
+		Player newPlayer =  PlayersRegister.getInstance().registerPlayer(userName, password);
 		ArrayList<parameterValue<?>> params = new ArrayList<MySQLHelper.parameterValue<?>>();
 		params.add(new MySQLHelper.parameterValue<String>(userName));
 		params.add(new MySQLHelper.parameterValue<String>(password));
@@ -55,7 +46,7 @@ public class LogonCommand implements Command {
 		try {
 			if (rs != null && rs.next()) {
 				Response r1 = new Response();
-				r1.setCommandID(id);
+				r1.setCommandID(super.getId());
 				r1.setUserId(rs.getInt("ID"));
 				r1.setMessage("Logon successfull");
 				
@@ -68,24 +59,5 @@ public class LogonCommand implements Command {
 		return returnValue;
 	}
 
-	@Override
-	public int getUserId() {
-		return userId;
-	}
-
-	@Override
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
-
-	@Override
-	public UUID getGameUid() {
-		return gameUid;
-	}
-
-	@Override
-	public void setGameUid(UUID gameUid) {
-		this.gameUid = gameUid;
-	}
 
 }
