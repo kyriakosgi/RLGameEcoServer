a8f2c1d0ba8b6816f3acc3130881ea7c422070f2
kyriakosgi <kyriakosgi@gmail.com>
2017-03-05 12:03:11 +0200

    Finalized implementation of execute()

--- RLGameEcoServer/src/gr/eap/RLGameEcoServer/comm/LogonCommand.java
+++ RLGameEcoServer/src/gr/eap/RLGameEcoServer/comm/LogonCommand.java
@@ -1,15 +1,10 @@
 package gr.eap.RLGameEcoServer.comm;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 import gr.eap.RLGameEcoServer.Player;
 import gr.eap.RLGameEcoServer.PlayersRegister;
-import gr.eap.RLGameEcoServer.db.MySQLHelper;
-import gr.eap.RLGameEcoServer.db.MySQLHelper.parameterValue;
 
 public class LogonCommand extends Command {
 	private String userName;
@@ -35,27 +30,16 @@ public class LogonCommand extends Command {
 	@Override
 	public List<Response> execute() {
 		Player newPlayer =  PlayersRegister.getInstance().registerPlayer(userName, password);
-		ArrayList<parameterValue<?>> params = new ArrayList<MySQLHelper.parameterValue<?>>();
-		params.add(new MySQLHelper.parameterValue<String>(userName));
-		params.add(new MySQLHelper.parameterValue<String>(password));
-
-		ResultSet rs = MySQLHelper.getInstance().query("Select * from players where Username = ? and Password = ?",
-				params);
 
 		ArrayList<Response> returnValue = new ArrayList<Response>();
-		try {
-			if (rs != null && rs.next()) {
+			if (newPlayer != null) {
 				Response r1 = new Response();
 				r1.setCommandID(super.getId());
-				r1.setUserId(rs.getInt("ID"));
+				r1.setUserId(newPlayer.getId());
 				r1.setMessage("Logon successfull");
 				
 				returnValue.add(r1);
 			}
-		} catch (SQLException e) {
-			System.err.println(e.getMessage());
-		}
-
 		return returnValue;
 	}
 
