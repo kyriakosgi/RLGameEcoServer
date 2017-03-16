bb78399d80066fa64c78057fc997f1ee435df857
kyriakosgi <kyriakosgi@gmail.com>
2017-03-17 00:07:24 +0200

    Implemented feature to update players connection info
    If a player was disconnected we add her with the new socketHash, so that
    she can continue playing

--- RLGameEcoServer/src/gr/eap/RLGameEcoServer/PlayersRegister.java
+++ RLGameEcoServer/src/gr/eap/RLGameEcoServer/PlayersRegister.java
@@ -26,6 +26,11 @@ public class PlayersRegister {
 		Player newPlayer = Player.getPlayer(userName, password);
 
 		if (newPlayer != null) {
+			//Add player to the connected players register along with his websocket hashcode
+			//If a player was disconnected we add her with the new socketHash, so that she can continue playing
+			if (players.containsKey(socketHash)){
+				players.remove(socketHash);
+			}
 			players.put(socketHash, newPlayer);
 		}
 
