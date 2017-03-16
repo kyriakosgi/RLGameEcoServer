54a0cb5ec9b95998ad4238eca899c07d0ed112b0
kyriakosgi <kyriakosgi@gmail.com>
2017-03-17 01:01:23 +0200

    Corrected the condition that creates the Response class

--- RLGameEcoServer/src/gr/eap/RLGameEcoServer/comm/CommObjectClassCreator.java
+++ RLGameEcoServer/src/gr/eap/RLGameEcoServer/comm/CommObjectClassCreator.java
@@ -4,7 +4,7 @@ public class CommObjectClassCreator {
 	public static Class<?> create(String commObjectType){
 		if (commObjectType.equals("gr.eap.RLGameEcoServer.comm.LogonCommand"))
 			return LogonCommand.class;
-		else if (commObjectType.equals("Response"))
+		else if (commObjectType.equals("gr.eap.RLGameEcoServer.comm.Response"))
 			return Response.class;
 		else
 			return null;
