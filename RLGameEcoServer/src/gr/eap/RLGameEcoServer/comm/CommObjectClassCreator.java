0da563d4769fb847700f69a9cb3aaae8a67ba45e
kyriakosgi <kyriakosgi@gmail.com>
2017-03-19 23:49:47 +0200

    Adapted code to the new response objects

--- RLGameEcoServer/src/gr/eap/RLGameEcoServer/comm/CommObjectClassCreator.java
+++ RLGameEcoServer/src/gr/eap/RLGameEcoServer/comm/CommObjectClassCreator.java
@@ -4,8 +4,10 @@ public class CommObjectClassCreator {
 	public static Class<?> create(String commObjectType){
 		if (commObjectType.equals("gr.eap.RLGameEcoServer.comm.LoginCommand"))
 			return LoginCommand.class;
-		else if (commObjectType.equals("gr.eap.RLGameEcoServer.comm.Response"))
+		else if (commObjectType.equals("gr.eap.RLGameEcoServer.comm.MessageResponse"))
 			return Response.class;
+		else if (commObjectType.equals("gr.eap.RLGameEcoServer.comm.PlayersListResponse"))
+			return PlayersListResponse.class;
 		else
 			return null;
 	}
