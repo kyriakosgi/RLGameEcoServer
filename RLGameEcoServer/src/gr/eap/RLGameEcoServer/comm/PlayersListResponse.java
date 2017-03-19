9cce667e9edca5c70a118febd6d0a4dbba3a2f49
kyriakosgi <kyriakosgi@gmail.com>
2017-03-19 23:53:13 +0200

    Created PlayersListResponse class

---
+++ RLGameEcoServer/src/gr/eap/RLGameEcoServer/comm/PlayersListResponse.java
@@ -0,0 +1,19 @@
+package gr.eap.RLGameEcoServer.comm;
+
+import java.util.ArrayList;
+
+import gr.eap.RLGameEcoServer.Player;
+import gr.eap.RLGameEcoServer.PlayersRegister;
+
+public class PlayersListResponse extends Response {
+	private ArrayList<Player> playersList = PlayersRegister.getInstance().getPlayersList();
+
+	public PlayersListResponse() {
+		this.setType("gr.eap.RLGameEcoServer.comm.PlayersListResponse");
+	}
+
+	public ArrayList<Player> getPlayersList() {
+		return playersList;
+	}
+
+}
