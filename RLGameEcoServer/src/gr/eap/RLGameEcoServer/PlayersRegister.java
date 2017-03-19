b442d482071d065d2aa1e8f2a61b27390faccd8e
kyriakosgi <kyriakosgi@gmail.com>
2017-03-19 23:58:19 +0200

    Created getPlayersList and sendPlayersList methods

--- RLGameEcoServer/src/gr/eap/RLGameEcoServer/PlayersRegister.java
+++ RLGameEcoServer/src/gr/eap/RLGameEcoServer/PlayersRegister.java
@@ -1,9 +1,12 @@
 package gr.eap.RLGameEcoServer;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.java_websocket.WebSocket;
+
+import gr.eap.RLGameEcoServer.comm.PlayersListResponse;
 
 
 public class PlayersRegister {
@@ -23,6 +26,10 @@ public class PlayersRegister {
 	public Map<Player, WebSocket> getPlayers() {
 		return players;
 	}
+	
+	public ArrayList<Player> getPlayersList(){
+		return new ArrayList<Player>(players.keySet());
+	}
 
 	public Player registerPlayer(String userName, String password, WebSocket socket) {
 		Player newPlayer = Player.getPlayer(userName, password);
@@ -32,12 +39,21 @@ public class PlayersRegister {
 			//If a player was disconnected we add her with the new socketHash, so that she can continue playing
 			if (players.containsKey(newPlayer)){
 				//TODO Send disconnect message
-				socket.close();
-				players.remove(socket);
+				players.get(newPlayer).close();
+				//players.remove(newPlayer);
 			}
 			players.put(newPlayer, socket);
+			sendPlayersList();
 		}
 
 		return newPlayer;
+	}
+	
+	private void sendPlayersList(){
+		PlayersListResponse r = new PlayersListResponse();
+		players.forEach((k,v) -> {
+			r.setSocket(v);
+			r.send();
+		});
 	}
 }
