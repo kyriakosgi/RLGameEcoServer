356a6f66ca9c7124b448c6bcdda0889d5e0b612f
kyriakosgi <kyriakosgi@gmail.com>
2017-03-25 00:25:51 +0200

    Added comments

--- RLGameEcoServer/src/gr/eap/RLGameEcoServer/comm/Command.java
+++ RLGameEcoServer/src/gr/eap/RLGameEcoServer/comm/Command.java
@@ -1,6 +1,6 @@
 package gr.eap.RLGameEcoServer.comm;
 
-
+// All commands will have data that come from the client in serialized JSON format and an execute method that will handle the data accordingly
 public abstract class Command extends CommunicationsObject{
 	private int id;
 	
