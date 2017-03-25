4cb1de0fce2dca1a3bf53c9c72763d428c714342
kyriakosgi <kyriakosgi@gmail.com>
2017-03-25 03:22:24 +0200

    Added the feature that the system can send a message to a socket
    Added extra constructors

--- RLGameEcoServer/src/gr/eap/RLGameEcoServer/Message.java
+++ RLGameEcoServer/src/gr/eap/RLGameEcoServer/Message.java
@@ -1,5 +1,7 @@
 package gr.eap.RLGameEcoServer;
 
+import org.java_websocket.WebSocket;
+
 import gr.eap.RLGameEcoServer.comm.MessageResponse;
 
 public class Message {
@@ -7,47 +9,68 @@ public class Message {
 		SYSTEM_INFO, SYSTEM_WARNING, SYSTEM_ALERT, USER_BROADCAST, USER_PERSONAL, USER_GAME, USER_TEAM
 	}
 
-	private String text;
-	private Type type;
-	private Player sender;
-	private Player recipient;
+	private String text = null;
+	private Type type = null;
+	private Player sender = null;
+	private Player recipient = null;
+	private transient WebSocket socket = null;
+
 
 	public String getText() {
 		return text;
 	}
 
+
 	public void setText(String text) {
 		this.text = text;
 	}
+
 
 	public Type getType() {
 		return type;
 	}
 
+
 	public void setType(Type type) {
 		this.type = type;
 	}
+
 
 	public Player getSender() {
 		return sender;
 	}
 
+
 	public void setSender(Player sender) {
 		this.sender = sender;
 	}
+
 
 	public Player getRecipient() {
 		return recipient;
 	}
 
+
 	public void setRecipient(Player recipient) {
 		this.recipient = recipient;
 	}
 
-	public Message() {
 
+	public WebSocket getSocket() {
+		return socket;
 	}
 
+
+	public void setSocket(WebSocket socket) {
+		this.socket = socket;
+	}
+
+
+	public Message() {
+	}
+
+	
+	//Standard message send by a player to another player
 	public Message(String text, Type type, Player sender, Player recipient) {
 		this.text = text;
 		this.type = type;
@@ -55,18 +78,43 @@ public class Message {
 		this.recipient = recipient;
 	}
 
+	//Message sent by the system to a player
+	public Message(String text, Type type, Player recipient) {
+		this.text = text;
+		this.type = type;
+		this.recipient = recipient;
+	}
+	
+	//Message sent by the system to a connection (non logged in client). A player cannot send such message.
+	public Message(String text, Type type, WebSocket socket) {
+		this.text = text;
+		this.type = type;
+		this.socket = socket;
+	}
+
+	
 	public void send() {
 		send(-1);
 	}
 
 	public void send(int commandId) {
-		MessageResponse response = new MessageResponse();
-		if (commandId>0) response.setCommandID(commandId);
-		response.setMessage(this);
-		response.setConnectionState(recipient.getConnectionState());
-		response.setSocket(recipient.getConnection());
-		response.setUserId(recipient.getId());
-		response.send();
+		if (text != null && !text.isEmpty() && type != null && (recipient != null || socket != null)) {
+			
+			MessageResponse response = new MessageResponse();
+			if (commandId>0) response.setCommandID(commandId);
+			response.setMessage(this);
+			if (recipient != null){
+				response.setConnectionState(recipient.getConnectionState());
+				response.setSocket(recipient.getConnection());
+				response.setUserId(recipient.getId());
+			}
+			else
+			{
+				response.setConnectionState(ConnectionState.CONNECTED); // if there is no recipient, means that the system sends message to a connected client
+				response.setSocket(socket);
+			}
+			response.send();
+		}
 
 	}
 }
