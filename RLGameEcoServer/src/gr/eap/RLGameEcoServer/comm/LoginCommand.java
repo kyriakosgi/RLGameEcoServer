6f2b0009c770604712a295e1598349773e95403d
kyriakosgi <kyriakosgi@gmail.com>
2017-03-25 03:29:50 +0200

    Changed the execute method to use the Message class functionality

--- RLGameEcoServer/src/gr/eap/RLGameEcoServer/comm/LoginCommand.java
+++ RLGameEcoServer/src/gr/eap/RLGameEcoServer/comm/LoginCommand.java
@@ -1,15 +1,14 @@
 package gr.eap.RLGameEcoServer.comm;
 
 
-import gr.eap.RLGameEcoServer.ConnectionState;
 import gr.eap.RLGameEcoServer.Message;
 import gr.eap.RLGameEcoServer.Player;
 import gr.eap.RLGameEcoServer.PlayersRegister;
 import gr.eap.RLGameEcoServer.Message.Type;
 
 public class LoginCommand extends Command {
-	private String userName;
-	private String password;
+	private String userName = null;
+	private String password = null;
 
 	public String getUserName() {
 		return userName;
@@ -35,20 +34,24 @@ public class LoginCommand extends Command {
 	public void execute() {
 		Player newPlayer = PlayersRegister.getInstance().registerPlayer(userName, password, getSocket());
 
+		Message message = new Message();
+		
+		
 		MessageResponse r1 = new MessageResponse();
 		r1.setCommandID(super.getId());
 		r1.setSocket(getSocket());
 		if (newPlayer != null) {
-			r1.setUserId(newPlayer.getId());
-			r1.setMessage(new Message("Login successfull",Type.SYSTEM_INFO));
-			r1.setConnectionState(ConnectionState.LOGGED_IN);
+			message.setRecipient(newPlayer);
+			message.setText("Login successfull");
+			message.setType(Type.SYSTEM_INFO);
 		}
 		else
 		{
-			r1.setMessage(new Message("Login failed", Type.SYSTEM_ALERT));
-			r1.setConnectionState(ConnectionState.CONNECTED);
+			message.setSocket(getSocket());
+			message.setText("Login failed");
+			message.setType(Type.SYSTEM_ALERT);
 		}
-		r1.send();
+		message.send(super.getId());
 	}
 
 }
