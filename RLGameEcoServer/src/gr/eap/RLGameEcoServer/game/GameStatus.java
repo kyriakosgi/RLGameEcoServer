bd320083fe03d465c61530ee7a904af8b073ada0
kyriakosgi <kyriakosgi@gmail.com>
2017-04-10 22:30:12 +0300

    Added the desciption property

--- RLGameEcoServer/src/gr/eap/RLGameEcoServer/game/GameStatus.java
+++ RLGameEcoServer/src/gr/eap/RLGameEcoServer/game/GameStatus.java
@@ -1,5 +1,19 @@
 package gr.eap.RLGameEcoServer.game;
 
 public enum GameStatus {
-	WAITING_SECOND_PLAYER, WAITING_INVITATION_ACCEPTANCE, IN_PROGRESS, INTERRUPTED, FINISHED
+	WAITING_SECOND_PLAYER ("Waiting for player to join"), 
+	WAITING_INVITATION_ACCEPTANCE ("Waiting for player to accept invitation"), 
+	IN_PROGRESS ("In progress"), 
+	INTERRUPTED ("Interrupted"), 
+	FINISHED ("Finished");
+	
+	String description;
+	
+	private GameStatus(String description){
+		this.description=description;
+	}
+	
+	public String getDescription(){
+		return description;
+	}
 }
