7db098a3b7a4ac7316a3f773c1a20b5561bbf99d
kyriakosgi <kyriakosgi@gmail.com>
2017-04-10 23:54:55 +0300

    Added SerializedName

--- RLGameEcoServer/src/gr/eap/RLGameEcoServer/game/GameStatus.java
+++ RLGameEcoServer/src/gr/eap/RLGameEcoServer/game/GameStatus.java
@@ -1,13 +1,25 @@
 package gr.eap.RLGameEcoServer.game;
 
+import com.google.gson.annotations.SerializedName;
+
 public enum GameStatus {
-	WAITING_SECOND_PLAYER ("Waiting for player to join"), 
+	
+	@SerializedName("Waiting for player to join")
+	WAITING_SECOND_PLAYER ("Waiting for player to join"),
+	
+	@SerializedName("Waiting for player to accept invitation")
 	WAITING_INVITATION_ACCEPTANCE ("Waiting for player to accept invitation"), 
+	
+	@SerializedName("In progress")
 	IN_PROGRESS ("In progress"), 
+	
+	@SerializedName("Interrupted")
 	INTERRUPTED ("Interrupted"), 
+	
+	@SerializedName("Finished")
 	FINISHED ("Finished");
 	
-	private String description;
+	private final String description;
 	
 	private GameStatus(String description){
 		this.description=description;
