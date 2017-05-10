0c9df1c94108519e6789009c873c345c05207f61
kyriakosgi <kyriakosgi@gmail.com>
2017-05-10 08:53:59 +0300

    Added a new constructor so that we can send an arbitrary list

--- RLGameEcoServer/src/gr/eap/RLGameEcoServer/comm/GamesListResponse.java
+++ RLGameEcoServer/src/gr/eap/RLGameEcoServer/comm/GamesListResponse.java
@@ -6,12 +6,18 @@ import gr.eap.RLGameEcoServer.game.Game;
 import gr.eap.RLGameEcoServer.game.GamesRegister;
 
 public class GamesListResponse extends Response {
-	private ArrayList<Game> gamesList = GamesRegister.getInstance().getGamesList();
+	private ArrayList<Game> gamesList;
 
 	public GamesListResponse() {
+		gamesList = GamesRegister.getInstance().getGamesList();
 		this.setType("gr.eap.RLGameEcoServer.comm.GamesListResponse");
 	}
 
+	public GamesListResponse(ArrayList<Game> gamesList) {
+		this.gamesList = gamesList;
+		this.setType("gr.eap.RLGameEcoServer.comm.GamesListResponse");
+	}
+	
 	public ArrayList<Game> getGamesList() {
 		return gamesList;
 	}
