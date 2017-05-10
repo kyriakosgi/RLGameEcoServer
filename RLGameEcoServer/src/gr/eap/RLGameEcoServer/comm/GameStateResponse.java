81b0542b33969140a20364f417a406fe5e46514b
kyriakosgi <kyriakosgi@gmail.com>
2017-05-11 00:01:05 +0300

    Added the gameUid property

--- RLGameEcoServer/src/gr/eap/RLGameEcoServer/comm/GameStateResponse.java
+++ RLGameEcoServer/src/gr/eap/RLGameEcoServer/comm/GameStateResponse.java
@@ -1,16 +1,24 @@
 package gr.eap.RLGameEcoServer.comm;
 
+import java.util.UUID;
+
 import org.rlgame.gameplay.GameState;
 
 public class GameStateResponse extends Response {
 	private GameState state;
+	private UUID gameUid;
+	
+	public UUID getGameUid() {
+		return gameUid;
+	}
 
 	public GameState getState() {
 		return state;
 	}
 	
-	public GameStateResponse(GameState state) {
+	public GameStateResponse(GameState state, UUID gameUid) {
 		this.state = state;
+		this.gameUid = gameUid;
 		this.setType("gr.eap.RLGameEcoServer.comm.GameStateResponse");
 	}
 
